f7f6d3fe86fe7182ffca1114902f54c10bc25d40
serght <sergioherrerat@javeriana.edu.co>
2025-06-16 10:51:16 -0500

    :bug:fix locales error

--- src/i18n.js
+++ src/i18n.js
@@ -2,8 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import translationEN from "./locales/en/translation.json";
-import translationES from "./locales/es/translation.json";
+import translationEN from "./Locales/en/translation.json";
+import translationES from "./Locales/es/translation.json";
 
 const resources = {
   en: {
